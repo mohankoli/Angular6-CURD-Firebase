3c76cc6fcb3f2bf55d3465b7e0c18f382ba1f451
Mohan Koli <mkoli@tibco.com>
2018-10-03 15:29:25 +0530

    first commit

---
+++ src/app/customer-list/customer-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../shared/customer.service';
+
+@Component({
+  selector: 'app-customer-list',
+  templateUrl: './customer-list.component.html',
+  styleUrls: ['./customer-list.component.css']
+})
+export class CustomerListComponent implements OnInit {
+
+  constructor(private customerService: CustomerService) { }
+  CustomerArray = [];
+  ngOnInit() {
+  
+    this.customerService.getCustomers().subscribe(
+    list =>{
+      this.CustomerArray = list.map(item =>{
+           return{
+             $key:item.key,
+             ...item.payload.val()
+           };
+      });
+    });
+  }
+  
+  updateCustomerData(customer){
+      this.customerService.populateForm(customer);
+   }
+   
+   
+   onDelete($key) {
+    if (confirm('Are you sure to delete this record ?')) {
+      this.customerService.deleteCustomer($key);
+      this.showDeletedMessage = true;
+      setTimeout(() => this.showDeletedMessage = false, 3000);
+    }
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyC8-eehuyRd91lYYsJW78QevsxaaQHy6Rk",
+    authDomain: "fir-curdapp-e3425.firebaseapp.com",
+    databaseURL: "https://fir-curdapp-e3425.firebaseio.com",
+    projectId: "fir-curdapp-e3425",
+    storageBucket: "fir-curdapp-e3425.appspot.com",
+    messagingSenderId: "750204636576"
+  }
+  
+  
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
